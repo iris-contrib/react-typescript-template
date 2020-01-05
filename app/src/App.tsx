4d113ec8ff84bbb94bdfc0dc7ce71f966626d9d0
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2020-01-05 16:18:23 +0200

    remove run.bat and run.sh as iris-cli now automatically can understand go-bindata assets to build and npm

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>src/App.tsx</code>
+           <code>app/src/App.tsx</code>
         </p>
         <a
           className="App-link"
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd app
-[ ! -d "./node_modules" ] && npm install
-yarn build
-cd ../
-go get -u github.com/go-bindata/go-bindata/...
-go-bindata -o bindata.go ./app/build/...
-go run .
