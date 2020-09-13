0fbbad166c7c7579df86101af5b18d85059af1ab
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2020-09-13 22:16:57 +0300

    iris v12.2.0-alpha

--- go.mod
+++ go.mod
@@ -3,5 +3,5 @@ module github.com/iris-contrib/react-typescript-template
 go 1.13
 
 require (
-	github.com/kataras/iris/v12 v12.1.6
+	github.com/kataras/iris/v12 v12.2.0-alpha
 )
--- main.go
+++ main.go
@@ -4,11 +4,9 @@ import "github.com/kataras/iris/v12"
 
 func main() {
 	app := iris.New()
-	app.HandleDir("/", "./app/build", iris.DirOptions{
-		Asset:      Asset,
-		AssetNames: AssetNames,
-		AssetInfo:  AssetInfo,
-	})
 
-	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+	// $ go-bindata -prefix "app/build" -fs ./app/build/...
+	app.HandleDir("/", AssetFile())
+
+	app.Listen(":8080")
 }
